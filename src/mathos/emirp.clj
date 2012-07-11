ed7a5b4c6ec8423091332b12f05cc117b0772b9f
Andrew Curry <andrew.d.curry@gmail.com>
2012-07-10 23:11:12 -0400

    Revert "cleaned up emirp"
    This reverts commit 290d9d290dd1417992bbc5198c24b818cd6c2fd5.

--- src/mathos/emirp.clj
+++ src/mathos/emirp.clj
@@ -5,17 +5,10 @@
   [seq elm]
   (some #(= elm %) seq))
 
-(defn to-numeric-palindrome
+(defn toNumericPalindrome
   "Returns a number's reversed digits as an Integer."
   [x]
   (Integer. (apply str (reverse (str x)))))
-
-(defn is-emirp? [n]
-  "Determines if number is an emirp."
-  (and
-    (not (s/palindromic? x))
-    (in? primes (to-numeric-palindrome x))
-    ))
 
 (defn emirp
   "Return a list of 'emirp' primes; primes whose reverse forms
@@ -25,7 +18,11 @@
     [primes (s/sieve upperBound)]
     (filter
       (fn [x]
-        (is-emirp? x) primes
+        (and
+          (not (s/palindromic? x))
+          (in? primes (toNumericPalindrome x))
+          )
+        ) primes
       )
     )
   )

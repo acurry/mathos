05dcaec947fde1e37cfce8c79139156c12e4b15c
Andrew Curry <acurry@texeltek.com>
2012-07-18 11:15:36 -0400

    got emirp and sieve working

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "math tools as a compojure web service"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [compojure "1.1.1"]
-                 [hiccup "1.0.0"]]
+                 [hiccup "1.0.0"]
+                 [org.clojure/data.json "0.1.2"]]
   :main mathos.core
   :ring {:handler mathos.routes/app})
--- src/mathos/routes.clj
+++ src/mathos/routes.clj
@@ -1,9 +1,28 @@
 (ns mathos.routes
   (:use compojure.core mathos.views)
-  (:require [compojure.route :as route]))
+  (:require [compojure.route :as route]
+            [mathos.core :as mathos]
+            [clojure.data.json :as cljson]))
+
+(defn with-json [data & [status]]
+  {:status (or status 200)
+   :headers {"Content-Type" "application/json"}
+   :body (cljson/json-str data)})
+
+(defroutes emirp-routes
+           (GET "/:n" 
+                [n] 
+                (with-json (mathos/emirp (Integer. n)))))
+
+(defroutes sieve-routes
+           (GET ["/:n" :n #"[0-9]+"] [n]
+                (with-json (mathos/sieve (Integer. n)))))
 
 (defroutes app
            (GET "/" [] (index-page))
+           (context "/emirp" [] emirp-routes)
+           (context "/sieve" [] sieve-routes)
+           (context "/primes" [] sieve-routes)
            (route/resources "/")
            (route/not-found "not found"))
 

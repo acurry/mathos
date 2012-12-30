b8472c197315e9b480c937607b6a49a18ada16ec
Andrew Curry <andrew.d.curry@gmail.com>
2012-12-30 02:54:46 -0500

    formatting changes

--- src/mathos/factorial.clj
+++ src/mathos/factorial.clj
@@ -2,8 +2,12 @@
 
 (defn factorial
   "Computes factorial of n, an integer."
-  ([n] (factorial n 1N))
-  ([n aux] (if (= n 0) aux (recur (dec n) (* aux n)))))
+  ([n] 
+   (factorial n 1N))
+  ([n aux] 
+   (if (= n 0) 
+     aux 
+     (recur (dec n) (* aux n)))))
 
 (defn fact-places
   "Computes the base-10 magnitude of a factorial."
